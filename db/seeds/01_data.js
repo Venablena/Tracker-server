ed19a8bb4d17660b7d49707610aa415e4aba763b
Lena Venable <venablena@gmail.com>
2017-11-17 07:48:32 -0800

    changed time and date format

--- db/seeds/01_data.js
+++ db/seeds/01_data.js
@@ -5,15 +5,15 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('logs').insert([
-        {date: '2017-11-10', time: '14:38:00', lat: 47.58221422, long: -122.28589817, tag: 'click'},
-        {date: '2017-11-10', time: '14:20:00', lat: 47.564222, long: -122.2799805, tag: 'click'},
-        {date: '2017-11-12', time: '11:14:00', lat: 47.6228691, long: -122.3282837, tag: 'click'},
-        {date: '2017-11-10', time: '15:07:00', lat: 47.5604571, long: -122.2990966, tag: 'double_click'},
-        {date: '2017-11-10', time: '14:26:00', lat: 47.57010029, long: -122.27833526, tag: 'double_click'},
-        {date: '2017-11-12', time: '10:21:00', lat: 47.5605252, long: -122.254494, tag: 'double_click'},
-        {date: '2017-11-10', time: '11:28:00', lat: 47.5604415, long: -122.2991629, tag: 'hold'},
-        {date: '2017-11-10', time: '19:04:00', lat: 47.5604485, long: -122.2990989, tag: 'hold'},
-        {date: '2017-11-12', time: '11:07:00', lat: 47.6208036, long: -122.3384203, tag: 'hold'}
+        {date: 'Nov 10 2017', time: '2:38 PM', lat: 47.58221422, long: -122.28589817, tag: 'click'},
+        {date: 'Nov 10 2017', time: '2:20 PM', lat: 47.564222, long: -122.2799805, tag: 'click'},
+        {date: 'Nov 12 2017', time: '11:14 PM', lat: 47.6228691, long: -122.3282837, tag: 'click'},
+        {date: 'Nov 10 2017', time: '3:07 PM', lat: 47.5604571, long: -122.2990966, tag: 'double_click'},
+        {date: 'Nov 10 2017', time: '2:26 PM', lat: 47.57010029, long: -122.27833526, tag: 'double_click'},
+        {date: 'Nov 12 2017', time: '10:21 PM', lat: 47.5605252, long: -122.254494, tag: 'double_click'},
+        {date: 'Nov 10 2017', time: '11:28 AM', lat: 47.5604415, long: -122.2991629, tag: 'hold'},
+        {date: 'Nov 10 2017', time: '7:04 PM', lat: 47.5604485, long: -122.2990989, tag: 'hold'},
+        {date: 'Nov 12 2017', time: '11:07 AM', lat: 47.6208036, long: -122.3384203, tag: 'hold'}
       ]).then(() => {
       return knex.raw(
         `SELECT setval('logs_id_seq', (SELECT MAX(id) FROM logs));`
