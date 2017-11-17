c9c00988164c4e11ef336ada54023bd1cc78cb8e
Lena Venable <venablena@gmail.com>
2017-11-16 20:05:40 -0800

    remove morgan again

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const parser = require('body-parser')
-const morgan = require('morgan')
 const cors = require('cors')
 
 app.disable('x-powered-by')
