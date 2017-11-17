440a7894c61f0de00a083c4f351baf3bf57a1b52
Lena Venable <venablena@gmail.com>
2017-11-17 08:03:44 -0800

    reformatting date and time table -again

--- db/migrations/03_logs.js
+++ db/migrations/03_logs.js
@@ -2,8 +2,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('logs', table => {
     table.increments().notNullable()
-    table.date('date').notNullable()
-    table.time('time').notNullable()
+    table.string('date').notNullable()
+    table.string('time').notNullable()
+    //table.date('date').notNullable()
+    //table.time('time').notNullable()
     table.double('long').notNullable()
     table.double('lat').notNullable()
     table.integer('location_id')
