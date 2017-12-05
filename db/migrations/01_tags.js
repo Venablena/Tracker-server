831b32859454993a6b63f404f8133015100b8ba7
Wes Reid <bwreid@gmail.com>
2017-12-05 13:46:35 -0800

    Update 01_tags.js

--- db/migrations/01_tags.js
+++ db/migrations/01_tags.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('tags', table =>{
+    // What happened to `.increments()` and `.timestamps()`?  :(
     table.string('hold').notNullable().defaultTo('negative')
     table.string('click').notNullable().defaultTo('neutral')
     table.string('double_click').notNullable().defaultTo('positive')
